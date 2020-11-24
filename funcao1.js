12c58eb3837a3a2f5427955e2b4b2918e44e877c
Matheus Holms <matheus_holms@hotmail.com>
2020-11-23 22:50:14 -0300

    aula funções 1

---
+++ funcao1.js
@@ -0,0 +1,16 @@
+//Função sem retorno
+function imprimirSoma(a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2)
+
+//Função com retorno
+function soma(a, b = 0){
+    return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
+console.log()
