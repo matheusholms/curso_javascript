5f78f75a5c9f7e4ebfb136db8e893805d8643dd5
Matheus Ferreira Santos Holms <matheusholms@NOVOTEMPO.NET>
2020-12-01 11:20:23 -0300

    Orientado a Objeto

---
+++ objeto/notacaoLiteral.js
@@ -0,0 +1,13 @@
+const a = 1
+const b = 2
+const c = 3
+
+const obj1 = { a, b, c }
+console.log(obj1)
+
+const nomeAttr = 'nota'
+const valorAttr = 7.87
+
+const obj3 = {}
+obj3[nomeAttr] = valorAttr
+console.log(obj3)
