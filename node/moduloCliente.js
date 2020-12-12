32a8039c111365e75049a0b458231d6d93dac23e
Matheus Ferreira Santos Holms <matheus_holms@hotmail.com>
2020-12-12 01:30:06 -0300

    Note

---
+++ node/moduloCliente.js
@@ -0,0 +1,11 @@
+const moduloa = require('./moduloa')
+const modulob = require('./modulob')
+
+console.log(moduloa.ola)
+console.log(moduloa.bemVindo)
+console.log(moduloa.ateLogo)
+console.log(moduloa)
+
+console.log(modulob.bomDia)
+console.log(modulob.boaNoite())
+console.log(modulob)
